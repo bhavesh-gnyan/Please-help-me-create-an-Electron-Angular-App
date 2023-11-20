16b344584ccd3f79d980e5de7babaef6bb44d532
Bhavesh Kumar Dinesh <bhavesh@gnyan.ai>
2023-11-20 19:37:30 +0530

    chatgpt response committed

---
+++ main(2023-11-20 14:07:29).js
@@ -0,0 +1,26 @@
+javascript
+const { app, BrowserWindow } = require('electron');
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadFile('dist/index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
